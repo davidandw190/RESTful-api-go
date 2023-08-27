bd4b6777f84164d4a0d2caf9f9c8f2a38e6cdbd9
Andrei-David Nan <nandavid190@gmail.com>
2023-08-27 21:34:43 +0300

    v0.1 - Boostrapped db configuration;

---
+++ db/db.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"log"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type DbInstance struct {
+	Db *gorm.DB
+}
+
+var Database DbInstance
+
+func ConnectDb() {
+	db, err := gorm.Open(sqlite.Open("api.db"), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to the database.. \n")
+		db.Logger = logger.Default.LogMode(logger.Info)
+		log.Println("Running migrations...")
+		//TODO: Add migrations
+	}
+}
--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func home(c *fiber.Ctx) error {
+	return c.SendString("You are in home!")
+}
 
 func main() {
 	app := fiber.New()
+
+	app.Get("/api", home)
+
+	log.Fatal(app.Listen(":3000"))
 
 }
