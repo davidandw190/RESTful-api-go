1756107ac8b26288a2a723e07dff554415d5dc3f
Andrei-David Nan <nandavid190@gmail.com>
2023-08-29 16:42:18 +0300

    v0.6 - Added GetUser by id;

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api", home)
 	app.Post("/api/users", routes.CreateUser)
 	app.Get("/api/users", routes.GetAllUsers)
+	app.Get("/api/users/:id", routes.GetUser)
 }
 
 func main() {
--- routes/user.serializer.go
+++ routes/user.serializer.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/davidandw190/RESTful-api-go/db"
@@ -42,6 +43,7 @@ func CreateUser(c *fiber.Ctx) error {
 	return c.Status(http.StatusCreated).JSON(responseUser)
 }
 
+// GetAllUsers returns a slice of serialised users
 func GetAllUsers(c *fiber.Ctx) error {
 	users := []models.User{}
 
@@ -55,4 +57,35 @@ func GetAllUsers(c *fiber.Ctx) error {
 	}
 
 	return c.Status(http.StatusOK).JSON(respUsers)
+}
+
+// GetUser returns a user in serialised form by id
+func GetUser(c *fiber.Ctx) error {
+	id, err := c.ParamsInt("id")
+
+	var user models.User
+
+	if err != nil {
+		return c.Status(http.StatusBadRequest).JSON(err.Error)
+	}
+
+	if err := findUser(id, &user); err != nil {
+		return c.Status(http.StatusNotFound).JSON(err.Error())
+	}
+
+	responseUser := CreateResponseUser(user)
+
+	return c.Status(http.StatusAccepted).JSON(responseUser)
+
+}
+
+func findUser(id int, user *models.User) error {
+	db.Database.Db.Find(&user, "id=?", id)
+
+	if user.ID == 0 {
+		return errors.New("User does not exist")
+	}
+
+	return nil
+
 }
