b6033b400e534338b1dc332817e6325975c5a207
Andrei-David Nan <nandavid190@gmail.com>
2023-09-03 19:39:17 +0300

    v0.18 - Added handler for updating  user order;

--- models/order.go
+++ models/order.go
@@ -3,10 +3,14 @@ package models
 import "time"
 
 type Order struct {
-	ID           uint64    `json:"id" gorm:"primaryKey"`
-	ProductRefer uint64    `json:"product_id"`
-	Product      Product   `gorm:"foreignKey:ProductReferer"`
-	UserRefer    uint64    `json:"user_id"`
-	User         User      `gorm:"foreignKey:UserReference"`
-	CreatedAt    time.Time `gorm:"autoCreateTime"`
+	ID        uint64    `json:"id" gorm:"primaryKey"`
+	UserRefer uint64    `json:"user_id"`
+	User      User      `gorm:"foreignKey:UserReference"`
+	Products  []Product `gorm:"many2many:order_products;"`
+	CreatedAt time.Time `gorm:"autoCreateTime"`
+}
+
+type OrderProduct struct {
+	OrderID   uint64
+	ProductID uint64
 }
